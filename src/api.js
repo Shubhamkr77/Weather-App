a379976b0ffa8102eaca270057e705ea6c6d7349
Shubhamkr77 <137918428+Shubhamkr77@users.noreply.github.com>
2024-07-16 00:25:50 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,14 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '8b62b27b30msh121709e8a2ba3b8p1fbb8ejsn98adbc1de402',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+		'X-RapidAPI-Key': 'askasjashjashjashjashjhsjh',
+		'X-RapidAPI-Host': 'ahjhsajhsjahsjashj.rapidapi.com'
 	}
 };
 
-export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const GEO_API_URL="https://wft-ghjhjghgfg.rapidapi.com/v1/geo";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "1651c09c3d4e0b62be27e653db748486"; 
+export const WEATHER_API_KEY = "shahjshajashajashjshjsaja";   
 // try {
 // 	const response = await fetch("/cities", options);
 // 	const result = await response.text();
